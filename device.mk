38ebbc746826d71f4e3e7bb08b9a2e8e63b44428
Debayan <kardebayan3@gmail.com>
2021-08-27 11:26:16 +0530

    OP4BFB: Build some audio blobs from source
    * Also build android.hardware.audio@4.0 and android.hardware.soundtrigger@2.1-impl from source
    
    Change-Id: I0a88aca5181ed581a1d6d061288de176851f8112
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,17 @@
 LOCAL_PATH := device/oppo/OP4BFB
 
 # Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio@4.0 \
+    android.hardware.soundtrigger@2.1-impl \
+    audio.a2dp.default \
+    audio.bluetooth.default \
+    audio.r_submix.default \
+    audio.usb.default \
+    audio_policy.stub \ 
+    libaudiopreprocessing \
+    libtinycompress
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_device.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
