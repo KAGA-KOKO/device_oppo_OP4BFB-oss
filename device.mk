9f8d5c6a43c1939d56c26526094e0cbbc7710666
Debayan Kar <kardebayan3@gmail.com>
2021-09-01 08:55:12 +0530

    OP4BFB: Build vendor variant of android.hardware.usb@1.1
    * Fixes :
    - F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.usb@1.1-service-mediatek": library "android.hardware.usb@1.1.so" not found: needed by main executable
    
    Change-Id: I860c9a7ec8ed771d0427604c3dc3f61e314ecc1c
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>
    Signed-off-by: Emmanuel <temp-email-for-oauth-neonzgamingyt@gitlab.localhost>

--- device.mk
+++ device.mk
@@ -165,6 +165,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.1.vendor
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
