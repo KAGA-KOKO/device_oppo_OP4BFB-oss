32b73b66a8a6d7babea32c7dcb9d05566c057743
BANRI <kotakandis185@gmail.com>
2025-06-17 16:02:05 +0000

    Revert "OP4BFB: Migrate to Health 2.1 HAL"
    reverts this commit e0a92180ab737dfdbc70343d4c70bab3852056c1,for lineage 16
    
    Signed-off-by: BANRI <kotakandis185@gmail.com>

--- device.mk
+++ device.mk
@@ -39,12 +39,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
     android.hardware.gatekeeper@1.0-impl
 
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/ACCDET.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/ACCDET.kl \
