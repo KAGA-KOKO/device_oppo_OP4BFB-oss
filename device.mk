c005cac527af7c8751b94e926b2402ac516f4bad
BANRI <kotakandis185@gmail.com>
2025-06-17 15:42:33 +0000

    Revert "OP4BFB: Build vendor variant of android.hardware.usb@1.1"
    This reverts commit 9f8d5c6a43c1939d56c26526094e0cbbc7710666.

--- device.mk
+++ device.mk
@@ -165,10 +165,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.1.vendor
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
