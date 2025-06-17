b6959b0935a52ff906fc9b4b4fb6cfffc7b66f84
BANRI <kotakandis185@gmail.com>
2025-06-17 15:36:57 +0000

    Revert "OP4BFB: Build RenderScript HAL"
    This reverts commit 1a7606618b81d9bc1974d338ebf0faf1715c7fb6.

--- device.mk
+++ device.mk
@@ -150,10 +150,6 @@ PRODUCT_PACKAGES += \
     fstab.mt6765 \
     ueventd.mt6765.rc
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/configstore@1.1.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/configstore@1.1.policy \
